5b2ea5dadbbdd8caf1bc05687734faf6d4d60d43
Sadeghi Morteza <0660@omidnet.local>
2025-07-30 19:39:06 +0330

    first commit

---
+++ controller_base.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "registery.hpp"
+#include "interface_adapter.hpp"
+
+class Controller: public InterfaceAdapter
+{
+public:
+	Controller() = default;
+};
---
+++ presenter_base.hpp
@@ -0,0 +1,8 @@
+#include "Registery.hpp"
+#include "interface_adapter.hpp"
+
+class Presenter: public InterfaceAdapter
+{
+public:
+	Presenter() = default;
+};
